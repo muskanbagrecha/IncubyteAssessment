54496cfa52729066d14451feb402f663e574886f
muskanbagrecha <muskanbagrecha04@gmail.com>
2024-12-27 18:27:56 +0530

    feature: handle 0 arguments

--- src/main/java/org/project/StringCalculator.java
+++ src/main/java/org/project/StringCalculator.java
@@ -4,6 +4,12 @@ package org.project;
 public class StringCalculator {
 
     public int add(String numbers){
+
+        //Return 0 for no arguments.
+        if(numbers.isEmpty()){
+            return 0;
+        }
+
         return 0;
     }
 }
--- src/test/java/org/project/StringCalculatorTest.java
+++ src/test/java/org/project/StringCalculatorTest.java
@@ -1,5 +1,16 @@
 package org.project;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StringCalculatorTest {
+    private final StringCalculator stringCalculator = new StringCalculator();
+
+    @Test
+    public void testZeroArguments(){
+        int sum = stringCalculator.add("");
+        assertEquals(0, sum);
+    }
 
 }
